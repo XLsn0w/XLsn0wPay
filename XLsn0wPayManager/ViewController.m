a97fbf57513d40d479aa2079ef8c3c2b6dd40657
xlsn0w <xlsn0w@outlook.com>
2017-11-07 11:56:41 +0800

    update

--- XLsn0wPayManager/ViewController.m
+++ XLsn0wPayManager/ViewController.m
@@ -47,15 +47,9 @@
 }
 
 - (IBAction)wechatpay:(id)sender {
-    //    NSLog(@"%@",[self jumpToBizPay]);
-    PayReq* req             = [[PayReq alloc] init];
-    //                req.partnerId           = [dict objectForKey:@"partnerid"];
-    //                req.prepayId            = [dict objectForKey:@"prepayid"];
-    //                req.nonceStr            = [dict objectForKey:@"noncestr"];
-    //                req.timeStamp           = stamp.intValue;
-    //                req.package             = [dict objectForKey:@"package"];
-    //                req.sign                = [dict objectForKey:@"sign"];
     
+    ///实际项目里面partnerId prepayId package nonceStr timeStamp sign 都是从服务器后台获取赋值给req的属性即可
+    PayReq *req = [[PayReq alloc] init];
     req.partnerId = @"10000100";
     req.prepayId= @"1101000000140415649af9fc314aa427";
     req.package = @"Sign=WXPay";
@@ -63,137 +57,15 @@
     req.timeStamp= @"1397527777".intValue;
     req.sign= @"582282D72DD2B03AD892830965F428CB16E7A256";
 
-    
     [XLsn0wPayManager xlsn0wPayWithOrder:req callBack:^(XLsn0wPayResult payResult, NSString *errorMessage) {
-        
         NSLog(@"errCode = %zd,errStr = %@",payResult, errorMessage);
-        
-        
     }];
 }
 
 
 - (IBAction)alipay:(id)sender {
-    //重要说明
-    //这里只是为了方便直接向商户展示支付宝的整个支付流程；所以Demo中加签过程直接放在客户端完成；
-    //真实App里，privateKey等数据严禁放在客户端，加签过程务必要放在服务端完成；
-    //防止商户私密数据泄露，造成不必要的资金损失，及面临各种安全风险；
-    /*============================================================================*/
-    /*=======================需要填写商户app申请的===================================*/
-    /*============================================================================*/
-    NSString *appID = @"";
-    
-    // 如下私钥，rsa2PrivateKey 或者 rsaPrivateKey 只需要填入一个
-    // 如果商户两个都设置了，优先使用 rsa2PrivateKey
-    // rsa2PrivateKey 可以保证商户交易在更加安全的环境下进行，建议使用 rsa2PrivateKey
-    // 获取 rsa2PrivateKey，建议使用支付宝提供的公私钥生成工具生成，
-    // 工具地址：https://doc.open.alipay.com/docs/doc.htm?treeId=291&articleId=106097&docType=1
-    NSString *rsa2PrivateKey = @"";
-    NSString *rsaPrivateKey = @"";
-    /*============================================================================*/
-    /*============================================================================*/
-    /*============================================================================*/
-    
-    //partner和seller获取失败,提示
-    if ([appID length] == 0 ||
-        ([rsa2PrivateKey length] == 0 && [rsaPrivateKey length] == 0))
-    {
-        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"提示"
-                                                        message:@"缺少appId或者私钥。"
-                                                       delegate:self
-                                              cancelButtonTitle:@"确定"
-                                              otherButtonTitles:nil];
-        [alert show];
-        return;
-    }
-    
-    /*
-     *生成订单信息及签名
-     */
-    //将商品信息赋予AlixPayOrder的成员变量
-    Order* order = [Order new];
-    
-    // NOTE: app_id设置
-    order.app_id = appID;
-    
-    // NOTE: 支付接口名称
-    order.method = @"alipay.trade.app.pay";
-    
-    // NOTE: 参数编码格式
-    order.charset = @"utf-8";
-    
-    // NOTE: 当前时间点
-    NSDateFormatter* formatter = [NSDateFormatter new];
-    [formatter setDateFormat:@"yyyy-MM-dd HH:mm:ss"];
-    order.timestamp = [formatter stringFromDate:[NSDate date]];
-    
-    // NOTE: 支付版本
-    order.version = @"1.0";
-    
-    // NOTE: sign_type 根据商户设置的私钥来决定
-    order.sign_type = (rsa2PrivateKey.length > 1) ? @"RSA2" : @"RSA";
-    
-    // NOTE: 商品数据
-    order.biz_content = [BizContent new];
-    order.biz_content.body = @"商品描述";
-    order.biz_content.subject = @"订单标题";
-    order.biz_content.out_trade_no = [self generateTradeNO]; //订单ID（由商家自行制定）
-    order.biz_content.timeout_express = @"30m"; //超时时间设置
-    order.biz_content.total_amount = [NSString stringWithFormat:@"%.2f", 0.01]; //商品价格
-    
-    //将商品信息拼接成字符串
-    NSString *orderInfo = [order orderInfoEncoded:NO];
-    NSString *orderInfoEncoded = [order orderInfoEncoded:YES];
-    NSLog(@"orderSpec = %@",orderInfo);
-    
-    // NOTE: 获取私钥并将商户信息签名，外部商户的加签过程请务必放在服务端，防止公私钥数据泄露；
-    //       需要遵循RSA签名规范，并将签名字符串base64编码和UrlEncode
-    NSString *signedString = nil;
-    RSADataSigner* signer = [[RSADataSigner alloc] initWithPrivateKey:((rsa2PrivateKey.length > 1)?rsa2PrivateKey:rsaPrivateKey)];
-    if ((rsa2PrivateKey.length > 1)) {
-        signedString = [signer signString:orderInfo withRSA2:YES];
-    } else {
-        signedString = [signer signString:orderInfo withRSA2:NO];
-    }
-    
-    // NOTE: 如果加签成功，则继续执行支付
-    if (signedString != nil) {
-        //应用注册scheme,在AliSDKDemo-Info.plist定义URL types
-        NSString *appScheme = @"alisdkdemo";
-        
-        // NOTE: 将签名成功字符串格式化为订单字符串,请严格按照该格式
-        NSString *orderString = [NSString stringWithFormat:@"%@&sign=%@",
-                                 orderInfoEncoded, signedString];
-        
-        // NOTE: 调用支付结果开始支付
-        [[AlipaySDK defaultService] payOrder:orderString fromScheme:appScheme callback:^(NSDictionary *resultDic) {
-            NSLog(@"reslut = %@",resultDic);
-            NSString *resultStatus = resultDic[@"resultStatus"];
-            NSString *errStr = resultDic[@"memo"];
-            
-            
-            NSLog(@"resultStatus= %@", resultStatus);
-            NSLog(@"errStr= %@", errStr);
-            
-            switch (resultStatus.integerValue) {
-                case 9000:// 成功
-                
-                
-                break;
-                
-                case 6001:// 取消
-                
-                
-                break;
-                default:
-                
-                
-                break;
-            }
-        }];
-    }
-
-//拼接订单信息并且签名后的字符串
+    ///拼接订单信息并且签名后的字符串
+    ///实际项目里这段签名字符串是从服务器后台获取
     NSString *orderMessage = @"app_id=2015052600090779&biz_content=%7B%22timeout_express%22%3A%2230m%22%2C%22seller_id%22%3A%22%22%2C%22product_code%22%3A%22QUICK_MSECURITY_PAY%22%2C%22total_amount%22%3A%220.02%22%2C%22subject%22%3A%221%22%2C%22body%22%3A%22%E6%88%91%E6%98%AF%E6%B5%8B%E8%AF%95%E6%95%B0%E6%8D%AE%22%2C%22out_trade_no%22%3A%22314VYGIAGG7ZOYY%22%7D&charset=utf-8&method=alipay.trade.app.pay&sign_type=RSA&timestamp=2016-08-15%2012%3A12%3A15&version=1.0&sign=MsbylYkCzlfYLy9PeRwUUIg9nZPeN9SfXPNavUCroGKR5Kqvx0nEnd3eRmKxJuthNUx4ERCXe552EV9PfwexqW%2B1wbKOdYtDIb4%2B7PL3Pc94RZL0zKaWcaY3tSL89%2FuAVUsQuFqEJdhIukuKygrXucvejOUgTCfoUdwTi7z%2BZzQ%3D";
 
     
